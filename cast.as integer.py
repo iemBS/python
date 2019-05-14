f8601e3e5f190a887841c71cc31aa752d4f2aecc
sburnell <sburnell@gmx.de>
2019-05-14 01:46:38 -0700

    Create cast.as integer.py

---
+++ cast.as integer.py
@@ -0,0 +1,17 @@
+# using integer
+x = int(1)
+
+# using float
+y = int(2.8)
+
+# using string version of an integer
+z = int("3")
+
+# return 1
+print(x)
+
+# return 2
+print(y)
+
+#return 3
+print(z)
