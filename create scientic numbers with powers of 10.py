d2e5b9d6d1bbd28c365ad5ee55a7f0e0a65c2cae
sburnell <sburnell@gmx.de>
2019-05-14 02:05:19 -0700

    Create create scientic numbers with powers of 10.py

---
+++ create scientic numbers with powers of 10.py
@@ -0,0 +1,9 @@
+# The e stands for Exponent, which means the number of tens you multiply a number by. For example, if I square 123456789, I get 1.524157875019e+16, which means that the answer is 1.524157875019 times 10 raised to the sixteenth power (that is, multiplied by 10 sixteen times)
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
