f77194cb06a8a4588953f7957b1855827bf68f7e
sburnell <sburnell@gmx.de>
2019-05-14 16:05:34 -0700

    Update obj.2.json.py
    added code

--- obj.2.json.py
+++ obj.2.json.py
@@ -79,3 +79,24 @@ x = {
 
 # use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
 print(json.dumps(x, indent=4, separators=(". ", " = ")))
+
+#
+# version 5, use sort_keys parameter
+#
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# sort the result alphabetically by keys:
+print(json.dumps(x, indent=4, sort_keys=True))
