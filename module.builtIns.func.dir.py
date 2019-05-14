de65007cbe0b465dee15c14daeaae08c67d02511
sburnell <sburnell@gmx.de>
2019-05-14 15:04:37 -0700

    Update and rename module.builtIns.method.dir.py to module.builtIns.func.dir.py
    updated file name and comment

--- module.builtIns.func.dir.py
+++ module.builtIns.func.dir.py
@@ -1,4 +1,4 @@
-# dir() returns properties, modules, and methods
+# dir() returns properties, modules, and functions
 # Without arguments, dir() returns the names you have defined currently
 # dir() does not return the names of built-in functions and variables. 
 # If you want to return built-in names, they are defined in the standard module "builtins".
