c605a2062c497d6e3868ef0b62d16be680c5f94f
sburnell <sburnell@gmx.de>
2019-05-14 16:04:38 -0700

    Update obj.2.json.py
    added more code

--- obj.2.json.py
+++ obj.2.json.py
@@ -1,4 +1,6 @@
+#
 # version 1
+#
 import json
 
 print(json.dumps({"name": "John", "age": 30}))
@@ -12,7 +14,9 @@ print(json.dumps(False))
 print(json.dumps(None))
 
 
+#
 # version 2
+#
 import json
 
 x = {
@@ -34,7 +38,9 @@ y = json.dumps(x)
 # the result is a JSON string:
 print(y)
 
+#
 # version 3, this uses the indent parameter
+#
 import json
 
 x = {
@@ -52,3 +58,24 @@ x = {
 
 # use four indents to make it easier to read the result:
 print(json.dumps(x, indent=4))
+
+#
+# version 4, change seperation parameter
+#
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
