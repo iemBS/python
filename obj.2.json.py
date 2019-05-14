81515455ec0ca990b7a83b4c040252b4d62bd246
sburnell <sburnell@gmx.de>
2019-05-14 16:01:58 -0700

    Update obj.2.json.py
    updated to have more code examples

--- obj.2.json.py
+++ obj.2.json.py
@@ -1,3 +1,4 @@
+# version 1
 import json
 
 print(json.dumps({"name": "John", "age": 30}))
@@ -9,3 +10,26 @@ print(json.dumps(31.76))
 print(json.dumps(True))
 print(json.dumps(False))
 print(json.dumps(None))
+
+
+# version 2
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
