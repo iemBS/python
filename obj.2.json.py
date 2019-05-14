acf89a9564170b1474d3759859436f02bda6fe69
sburnell <sburnell@gmx.de>
2019-05-14 16:02:41 -0700

    Update obj.2.json.py

--- obj.2.json.py
+++ obj.2.json.py
@@ -33,3 +33,22 @@ y = json.dumps(x)
 
 # the result is a JSON string:
 print(y)
+
+# version 3, this uses the indent parameter
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use four indents to make it easier to read the result:
+print(json.dumps(x, indent=4))
