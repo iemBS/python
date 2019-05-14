06ad8c9706db23d75735001e7945002c14f5523f
sburnell <sburnell@gmx.de>
2019-05-14 13:26:48 -0700

    Create collection.set.create.py

---
+++ collection.set.create.py
@@ -0,0 +1,8 @@
+# A set is a collection which is unordered and unindexed.
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+# Note: the set list is unordered, meaning: the items will appear in a random order.
+
+# run code again to see the result order change
