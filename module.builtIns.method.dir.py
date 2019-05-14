abcf8d1ed804066fd88e7cbac7dd45fdd2f80ab2
sburnell <sburnell@gmx.de>
2019-05-14 14:43:37 -0700

    Create module.builtIns.method.dir.py

---
+++ module.builtIns.method.dir.py
@@ -0,0 +1,14 @@
+# dir() returns properties, modules, and methods
+# Without arguments, dir() returns the names you have defined currently
+# dir() does not return the names of built-in functions and variables. 
+# If you want to return built-in names, they are defined in the standard module "builtins".
+
+
+import platform
+
+x = dir(platform)
+print(x)
+
+y = dir(builtins)
+print(y)
+
