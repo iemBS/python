8d76289656b0d9e17579d2cd54d16a9c1f3141a0
sburnell <sburnell@gmx.de>
2019-05-14 23:19:12 -0700

    Update db.mongoDB.collection.sort.py

--- db.mongoDB.collection.sort.py
+++ db.mongoDB.collection.sort.py
@@ -1,3 +1,4 @@
+# version 1
 import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -8,5 +9,18 @@ mydoc = mycol.find().sort("name")
 
 for x in mydoc:
   print(x)
+
+# version 2
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+mydoc = mycol.find().sort("name", -1)
+
+for x in mydoc:
+  print(x)
+
 
 
