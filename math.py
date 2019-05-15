3d2b82737921b9d6455dda492eb38c1f05a7194a
sburnell <sburnell@gmx.de>
2019-05-14 23:44:01 -0700

    Create math.py

---
+++ math.py
@@ -0,0 +1,26 @@
+ Maths: Requires import math
+
+    Absolute Value: a = abs(-7.5)
+    Arc sine: x = asin(0.5) #returns in rads
+    Ceil (round up): print(ceil(4.2))
+    Cosine: a = cos(x) #x in rads
+    Degrees: a = degrees(asin(0.5)) #a=30
+    Exp: y = exp(x) #y=e^x
+    Floor (round down): a = floor(a+0.5)
+    Log: x = log(y); #Natural Log
+       x = log(y,5); #Base-5 log
+    Log Base 10: x = log10(y)
+    Max: mx = max(1, 7, 3, 4) #7
+       mx = max(arr) #max value in array
+    Min: mn = min(3, 0, -1, x) #min value
+    Powers: x = pow(y,3) #x=y^3
+    Radians: a = cos(radians(60)) #a=0.5
+    Random #: Random number functions require import random
+       random.seed() #Set the seed based on the system time.
+       x = random() #Random number in the range [0.0, 1.0)
+       y = randint(a,b) #Random integer in the range [a, b]
+    Round: print round(3.793,1; #3.8 - rounded to 1 decimal
+       a = round(3.793,0) #a=4.0
+    Sine: a = sin(1.57) #in rads
+    Square Root: x = sqrt(10) #3.16...
+    Tangent: print tan(3.14)# #in rads 
