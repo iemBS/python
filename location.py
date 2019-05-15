61d5f4b9fe12b28ec80cf863652a0b45a15b6afb
sburnell <sburnell@gmx.de>
2019-05-15 01:02:02 -0700

    Update location.py
    added note

--- location.py
+++ location.py
@@ -5,3 +5,6 @@ location = GoogleV3().geocode(place)
 
 print(location.address)
 print(location.location)
+
+
+# There’s also a useful distance class. It calculates the distance between two locations in your favorite unit of measurement.
