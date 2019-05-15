a9c11ac0351ca8a3e1dec2af2fd5da16287d9e5a
sburnell <sburnell@gmx.de>
2019-05-15 00:03:41 -0700

    Update file.read.byLine.loop.py
    updated content

--- file.read.byLine.loop.py
+++ file.read.byLine.loop.py
@@ -1,3 +1,7 @@
 f = open("demofile.txt", "r")
 for x in f:
   print(x)
+  
+EOF: end-of-file is reached when read or readline returns an empty string. while (s != ""):
+   s = f.readline()
+   do_something
