c0701a33d632659ac9fdc68d930b9aa995bb643d
sburnell <sburnell@gmx.de>
2019-05-15 00:01:30 -0700

    Create collection.list.reshape.py

---
+++ collection.list.reshape.py
@@ -0,0 +1,3 @@
+reshape can be used to convert a 1-d array into a multi-dimensional array. To create a 5x5 array with the elements numbered from 0 to 24, you could use:
+b = arange(25)
+b = reshape(b,5,5)
