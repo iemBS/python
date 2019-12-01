8bab5b55492d9b23fb7e28452d31ed9bda2179b7
sburnell <1821834+sburnell@users.noreply.github.com>
2019-11-30 20:59:02 -0800

    Update condition.if.short.py

--- condition.if.short.py
+++ condition.if.short.py
@@ -1,4 +1,15 @@
+Shorten If Condition
+Notes:
+  -Alt version can be used in middle of other code.
+Main Success Scenario:
+  1. Put on single line 
+
 a = 200
 b = 33
 
-if a > b: print("a is greater than b")
+if a > b: x = 10
+
+Alternatives:
+  1a. Use shorthand
+    1a1.
+      x = 10 if a > b else 11
