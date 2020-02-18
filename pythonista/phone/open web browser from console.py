0083b271a8846a6145fbdbf33792a352fade0622
sburnell <1821834+sburnell@users.noreply.github.com>
2020-02-17 18:06:34 -0800

    Create open web browser from console.py

---
+++ pythonista/phone/open web browser from console.py
@@ -0,0 +1,10 @@
+Open Web Browser from Console
+Note:
+  -Web browser opens in another tab of Pythonista 
+Main Success Scenario:
+  1. See code below
+
+import webbrowser, os
+webbrowser.open("http:www.google.com")
+
+print("after browser opened")
