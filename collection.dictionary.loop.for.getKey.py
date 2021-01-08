5ee1348b7413f57263c35647ed78d01be0e78b74
sburnell <1821834+sburnell@users.noreply.github.com>
2021-01-08 09:40:08 -0800

    Update collection.dictionary.loop.for.getKey.py

--- collection.dictionary.loop.for.getKey.py
+++ collection.dictionary.loop.for.getKey.py
@@ -1,7 +1,14 @@
-thisdict =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-for x in thisdict:
-  print(x)
+Loop Through a Dictionary to get a key
+Note:
+    -Guidance @ 
+      -https://realpython.com/iterate-through-dictionary-python/
+Main Success Scenario:
+    1. See code below
+
+      thisdict =	{
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+      }
+      for x in thisdict:
+        print(x)
