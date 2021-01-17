eea2fb50d7495aa4f6d1068b1a371463b2dae8df
sburnell <1821834+sburnell@users.noreply.github.com>
2021-01-17 12:36:17 -0800

    Update tmp.py
    several code and comment updates

--- tmp.py
+++ tmp.py
@@ -7,17 +7,18 @@ import re
 import datetime
 import calendar
 import json
-import urllib
-import urllib3
-import urllib.request as req
+#import urllib # CAN I DROP?
+import urllib.request as reqCls
+#import urllib3 # CAN I DROP?
 from pytz import timezone
 import webbrowser, os # to open browser
 import configparser # to use config file
 
+# This makes use of the classes in this .PY file and any classes imported
 def main():
   print("hello")
 
-
+# Indicates the "main" method will be called when this .PY file is executed without any parameters
 if __name__ == "__main__":
   main()
 
@@ -26,7 +27,7 @@ class Url:
   # class attribute 
     # type 4 ex: amazon.com/Camera-Recorder-Fuvision-Portable-Recording/dp/B07GRRLCYS/ref=pd_aw_sbs_421_14
   urlVarType = {
-    "1":"URL vars start after '?', separate with '&', and assign value with '=',
+    	"1":"URL vars start after '?', separate with '&', and assign value with '=',
   	"2":"URL vars start after '?rh=', separate with ',', and assign value with ':'",
   	"3":"URL var value starts after '/'",
   	"4":"URL var value starts adter '/ref='"
@@ -39,31 +40,110 @@ class Url:
   	"3":"4"
   }
   
+  
   @staticmethod
   def getDomainNm(url):
     return url.split("/")[0]
     
     
-  def chkSafeUrl(url):
+  @staticmethod
+  def chkIsSafeUrl(url):
     return 0
     
+  
+  @staticmethod  
+  def chkIsUrlRedirect(url)
+    # Possible headers, ref @ https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+    # Headers my current browser is sending, ref @ https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending
+    headers = {} # Ref @ https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
     
-  def chkUrlRedirect(url)
-    return 0
+    # "url" param must have http or https prefix. If not, it throws an error.
+    req = reqCls.Request(url=url, headers=headers) # ref @ https://docs.python.org/3/library/urllib.request.html
+    resp = reqCls.urlopen(req, timeout=3) # ref @ https://docs.python.org/3/library/urllib.request.html
+    redirected = resp.geturl() != url 
+    return redirected
     
-    
+  
+  @staticmethod
   def seeTrackerUsed(url):
     return 0
     
     
-  def seeMyAccessibleInfo(url):
+  # If a site has my IP address they can check it against a IP to Geo Location map database
+  @staticmethod
+  def chkMyIpInGeoIpDb():
+    # Known GeoIpDB 
+    GeoIpDb = [
+      "maxmind.com"
+    ]
+    
+    # Attributes the database will have, if they have info
+    chkAtt = [
+      "Country",
+      "State",
+      "City",
+      "Zip",
+      "Lat & Long",
+      "ISP"
+    ]
+    
     return 0
     
     
-  def chkSafeNews(url):
+  # Check if my info is accessible by site, ref @ whatismybrowser.com
+  @staticmethod
+  def chkMyInfoAccessible(url):
+    chkAtt = [
+      "device", # Can be used for marketing purposes
+      "OS", # Can be used to take advantage of vulnerabilities
+      "Browser", # Can be used to take advantage of vulnerabilities
+      "IP", # Can be used to find out where I currently am and relate different sites visited
+      "IP on my network", # Can collect more detailed info on me by collecting my more specific IP from the sites I have visited. 
+      "DO NOT TRACK Enabled", # Indicates if path open to collect further info from me
+      "Cookies Enabled", # Indicates if path open to collect further info from me
+      "3rd Party Cookies Enabled", # Indicates if path open to collect further info from me
+      "Browser add-ons", # Indicates if path open to collect further info from me
+      "TOR Enabled" # Many sites will choose to block you, if you are using TOR (which hides your location)
+    ] 
+  '''
+    Sent by my browser
+      -Browser 
+      -OS
+      -device 
+      -IP address
+      -If JavaScript is enabled in browser
+      -If Cookies are enabled in browser
+      -If 3rd party cookies are enabled in browser
+      -City
+      -ISP
+      -screen resolution
+      -screen color depth
+      -browser window size
+      -If DO NOT TRACK is on
+      -If Browser has add-ons and what those add-ons are
+      -If using TOR
+  '''
     return 0
-  	
+    
+    
+  @staticmethod
+  def chkIsSafeNews(url):
+    return 0
+  
+  
+  @staticmethod
   def cleanUrl(url):
+    return 0
+   
+  
+  @staticmethod
+  def chkIsUrlFormat(url):
+    return 0
+    
+  
+  # Indicates if a secure version of the URL exists 
+  @staticmethod
+  def ifSecureUrlExists():
     return 0
   	
   	
@@ -121,14 +201,16 @@ class Url:
       return 0
   
   
-  def rmvUrlVar(url):
-	  if chkUrlVarType1(url) == 1:
-	    url = rmvUrlVarType1(url)
-	  if chkUrlVarType2(url) == 1:
-	    url = rmvUrlVarType2(url)
-	  if chkUrlVarType3(url) == 1:
-	    url = rmvUrlVarType3(url)
-	  return url 
+  def rmvUrlVar(url):  
+    if chkUrlVarType1(url) == 1:
+      url = rmvUrlVarType1(url)
+    if chkUrlVarType2(url) == 1:
+      url = rmvUrlVarType2(url)
+    if chkUrlVarType3(url) == 1:
+      url = rmvUrlVarType3(url)
+    if chkUrlVarType4(url) == 1:
+      url = rmvUrlVarType4(url)
+    return url 
   
   
   def rmvUrlVarType1(url,urlVarsStr):
@@ -136,16 +218,22 @@ class Url:
 	  urlFix = url.split("?")[0] + ("?" if len(urlVarsStr) > 0 else "") + urlVarsStr[0:len(urlVarsStr)]
 	  return urlFix
 	  
+	  
   def rmvUrlVarType1Auto(url):
     return urlVarsStr
 
+
   def rmvUrlVarType1Manual(url):
     return urlVarsStr
-	    
+	 
+	 
   def rmvUrlVarType2(url):
 
       
   def rmvUrlVarType3(url):
+  
+  
+  def rmvUrlVarType4(url):
 
 
 # Uses Url class in this file
